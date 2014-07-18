5ead65be92c14f4f975bbd932df53dcce10bae60
MaikSwitalski <strg.alt.entf@gmx.net>
2014-07-19 01:35:23 +0200

    fast fertig oder so? kp....

--- Phoneword_Droid/MainActivity.cs
+++ Phoneword_Droid/MainActivity.cs
@@ -16,16 +16,22 @@ namespace Phoneword_Droid
 	public class MainActivity : Activity
 	{
 
-		EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
-		Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+		EditText phoneNumberText;
+		Button translateButton; 
+		Button callButton;
+
+		private String callButtonPrefix = "Call ";
 
 
 
-		int count = 1;
+
+		//int count = 1;
 
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
+
+
 
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
@@ -33,8 +39,79 @@ namespace Phoneword_Droid
 			// Get our button from the layout resource,
 			// and attach an event to it
 
+			guiElementeZuordnen ();
+		
+			addTranslateListener ();
+			addCallListener ();
+
+			callButton.Enabled = false;
+
+
 		}
+
+
+		private void guiElementeZuordnen()
+		{
+			phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+			translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+			callButton = FindViewById<Button> (Resource.Id.CallButton);
+		}
+
+
+		private void addTranslateListener()
+		{
+
+			string translatedNumber = string.Empty;
+
+			translateButton.Click += (object sender, EventArgs e) => 
+			{
+				translatedNumber = Core.PhoneTranslator.translate(phoneNumberText.Text);
+				if(String.IsNullOrWhiteSpace(translatedNumber))
+					{
+						callButton.Enabled = false;
+						callButton.Text = "Call";
+					}
+					else
+					{
+						callButton.Enabled = true;
+					callButton.Text = callButtonPrefix + translatedNumber;
+
+					}
+			};
+
+		}
+
+
+		private void addCallListener()
+					{
+
+						callButton.Click += delegate {
+
+							var calldialogConfirm = new AlertDialog.Builder(this);
+
+				String number = callButton.Text.Substring(callButtonPrefix.Length);
+				calldialogConfirm.SetMessage("Realy call " + number + " ?");
+				calldialogConfirm.SetNeutralButton("JA", delegate {
+					Toast callDummy = new Toast(this);
+					callDummy.SetText("normalerweise würde ich nun callen");
+					callDummy.Show();
+				});
+				calldialogConfirm.SetNeutralButton("Cancel", delegate {});
+
+
+				calldialogConfirm.Show();
+
+						};
+
+
+					}
+
+
+
+
+
 	}
+	
 }
 
 
---
+++ Phoneword_Droid/PhoneTranslator.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+using System;
+
+namespace Core
+{
+	public static class PhoneTranslator
+	{
+		private static String numberVar = " -0123456789";
+
+		public static String translate (String raw)
+		{
+			if (string.IsNullOrWhiteSpace (raw))
+				return "";
+			else
+				raw = raw.ToUpperInvariant();
+
+
+			var newNumber = new StringBuilder ();
+
+			foreach (var c in raw) {
+				String cString = c.ToString ();
+				if (numberVar.Contains (cString))
+					newNumber.Append (c);
+				else {
+					var result = TranslateToNumber( cString);
+					if (result != null)
+						newNumber.Append (result);
+					// otherwise we've skipped aF non-numeric char
+				}
+
+
+
+			}
+			return newNumber.ToString();
+		}
+
+
+
+		private static int? TranslateToNumber(String c)
+		{
+			if ("ABC".Contains(c))
+				return 2;
+			else if ("DEF".Contains(c))
+				return 3;
+			else if ("GHI".Contains(c))
+				return 4;
+			else if ("JKL".Contains(c))
+				return 5;
+			else if ("MNO".Contains(c))
+				return 6;
+			else if ("PQRS".Contains(c))
+				return 7;
+			else if ("TUV".Contains(c))
+				return 8;
+			else if ("WXYZ".Contains(c))
+				return 9;
+			return null;
+
+		}
+	}
+}
+
