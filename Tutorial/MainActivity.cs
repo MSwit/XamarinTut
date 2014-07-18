9968b5fced5703c5afeefc1e89e417e484a735e5
MaikSwitalski <strg.alt.entf@gmx.net>
2014-07-18 22:50:50 +0200

    test comment hinzugefügt

--- Tutorial/MainActivity.cs
+++ Tutorial/MainActivity.cs
@@ -17,7 +17,7 @@ namespace Tutorial
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
-
+			//test!!!!!!!
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
 
